1128bc7e826f12ca246570bb1ea7d54b7f3bf2fd
Lockboxapp <dgarrett.atl@gmail.com>
2025-10-21 22:37:44 -0400

    Fixed useRef

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useMemo, useState, useEffect } from "react";
+import React, { useMemo, useState, useEffect, useRef } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 import {
   PiggyBank,
